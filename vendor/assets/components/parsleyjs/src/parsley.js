1c776bedda5d171bc609723bf375b2f72a4ad529
vishaltps <vishalsadriya.tps@gmail.com>
2017-12-07 18:41:48 +0530

    commend parslet js code

--- vendor/assets/components/parsleyjs/src/parsley.js
+++ vendor/assets/components/parsleyjs/src/parsley.js
@@ -1,10 +1,10 @@
-import $ from 'jquery';
-import Parsley from './parsley/main';
-import './parsley/pubsub';
-import './parsley/remote';
-import './i18n/en';
-import inputevent from './vendor/inputevent';
+// import $ from 'jquery';
+// import Parsley from './parsley/main';
+// import './parsley/pubsub';
+// import './parsley/remote';
+// import './i18n/en';
+// import inputevent from './vendor/inputevent';
 
-inputevent.install();
+// inputevent.install();
 
-export default Parsley;
+// export default Parsley;
