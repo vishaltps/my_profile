3aaaefd553b7c170868fe04dd475eeef65bc5e87
vishaltps <vishalsadriya.tps@gmail.com>
2017-12-07 18:39:12 +0530

    Resolve issue of font awesome

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -61,7 +61,7 @@ Rails.application.configure do
   # config.active_job.queue_adapter     = :resque
   # config.active_job.queue_name_prefix = "my_profile_#{Rails.env}"
   config.action_mailer.delivery_method = :smtp
-  config.action_mailer.default_url_options = { host:'localhost', port: 3000 }
+  config.action_mailer.default_url_options = { host:'https://imvishal.herokuapp.com/' }
   config.action_mailer.perform_deliveries = true
   config.action_mailer.raise_delivery_errors = true
   config.action_mailer.default :charset => "utf-8"
