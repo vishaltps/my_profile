38ea8a4450b2385ccd18f3c338408d8cd14b2217
Vishal <vishal.sadariya@tatvasoft.com>
2018-08-06 11:55:52 +0530

    precompile assets

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -28,8 +28,11 @@ Rails.application.configure do
   # config.assets.css_compressor = :sass
 
   # Do not fallback to assets pipeline if a precompiled asset is missed.
-  config.assets.compile = false
+  # config.assets.compile = false
   config.serve_static_files = true
+  config.serve_static_assets = true
+  config.assets.compile = true
+
 
   # `config.assets.precompile` and `config.assets.version` have moved to config/initializers/assets.rb
 
